0c6978efdce369e05d600dd9aefd2e5765ad93a5
ofir nahmani <Ofirnahmani20@gmail.com>
2026-08-15 20:53:41 +0300

    Add histogram visualization to test.py using matplotlib and numpy

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
 print("hia all")
+import matplotlib.pyplot as plt
+import numpy as np
+
+# יצירת נתונים אקראיים לדוגמה
+data = np.random.randn(1000)
+
+plt.hist(data, bins=30, color='skyblue', edgecolor='black')
+plt.title('היסטוגרם')
+plt.xlabel('ערך')
+plt.ylabel('תדירות')
+plt.show()
